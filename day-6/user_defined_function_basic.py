7892098b712a7d8ea31557b7833c10fd045b0be8
Shrijan Lakhey <lakheyshrijan@gmail.com>
2023-08-22 16:03:20 +0545

    completed day-6, learned about user defined functions, while loop and solved some challenges from reeborg's world

---
+++ day-6/user_defined_function_basic.py
@@ -0,0 +1,6 @@
+# defining a function
+def my_function():
+    print("Hello World")
+
+# calling the function
+my_function()
---
+++ day-6/while_loop.py
@@ -0,0 +1,12 @@
+# syntax
+# while something_is_true:
+    # do this 
+
+num = 6
+while num > 0:
+    print(num)
+    num -= 1
+
+# when to use while or for loop?
+# use for loops when you have to iterate over something and need to do something with each thing you are iterating over
+# use while loop when you dont care what number in a sqeuence you are in, which item you are iterating through in a list and want to carry out ceratin functionality as many times as required until the condition set is satisfied 
