1795fb34938819bd212ef318a6174ab996d84cda
Shrijan Lakhey <lakheyshrijan@gmail.com>
2023-10-31 19:40:36 +0545

    added functionality to store the highest score locally

--- 020-021/snake_game/scoreboard.py
+++ 020-021/snake_game/scoreboard.py
@@ -7,21 +7,39 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score=0
+        with open("020-021/snake_game/data.txt") as data:
+            self.highscore=int(data.read())
         self.penup()
         self.color("white")
         self.setposition(x=0,y=260)
-        self.write(arg=f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
         self.hideturtle()
-        
+        self.update_scoreboard()
     
     def update_scoreboard(self):
-        self.write(arg=f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(arg=f"Score: {self.score} High Score: {self.highscore}", move=False, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
     
-    def game_over(self):
-        self.setposition(0,0)
-        self.write(arg="GAME OVER.", move=False, align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.setposition(0,0)
+    #     self.write(arg="GAME OVER.", move=False, align=ALIGNMENT, font=FONT)
+    
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+
+            with open("020-021/snake_game/data.txt", mode="w") as data:
+                data.write(f"{self.highscore}") # writing a string to the file
+                
+        self.score = 0
+        self.update_scoreboard()
+        
+            
+
+
+        
+            
