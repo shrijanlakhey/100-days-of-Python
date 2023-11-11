1ead6e55f949358189a2113afbf95d75bf000bfb
Shrijan Lakhey <lakheyshrijan@gmail.com>
2023-11-11 20:35:03 +0545

    added error handling functionality in NATO alphabet program

--- 026/NATO_alphabet/main.py
+++ 026/NATO_alphabet/main.py
@@ -9,9 +9,15 @@ nato_data_frame = pandas.read_csv("026/NATO_alphabet/nato_phonetic_alphabet.csv"
 phonetic_dict = {row.letter:row.code for (index, row) in nato_data_frame.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in alphabet please.")
+        generate_phonetic() # if exception arises, the function will be called again and everything will run from the start again
+        
+    else:
+        print(output_list)
 
-
-
+generate_phonetic()
